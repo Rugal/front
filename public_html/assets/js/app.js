54159668fa80b13c45c961354235d1814d184d47
Rugal <rugal.bernstein.0@gmail.com>
2016-02-10 12:58:28 -0500

    add admin.css

--- public_html/assets/js/app.js
+++ public_html/assets/js/app.js
@@ -5,5 +5,5 @@ imsApp.controller('indexController', ['$scope', function ($scope) {
         $scope.student='student.html';
         $scope.admin='admin.html';
         $scope.login='login.html';
-        $scope.current=$scope.login;
+        $scope.current=$scope.admin;
     }]);

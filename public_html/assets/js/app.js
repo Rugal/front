12f2b369e96a7385f6088b752e646ebec3e1cc71
Rugal <rugal.bernstein.0@gmail.com>
2016-02-14 00:38:59 -0500

    test for LZ

--- public_html/assets/js/app.js
+++ public_html/assets/js/app.js
@@ -1,9 +1,26 @@
 imsApp = angular.module('imsApp', []);
 
 
-imsApp.controller('indexController', ['$scope', function ($scope) {
-        $scope.student='student.html';
-        $scope.admin='admin.html';
-        $scope.login='login.html';
-        $scope.current=$scope.admin;
+imsApp.controller('indexController', ['$scope', '$http', function ($scope, $http) {
+        $scope.student = 'student.html';
+        $scope.admin = 'admin.html';
+        $scope.login = 'login.html';
+        $scope.current = $scope.admin;
+    }]);
+
+
+imsApp.controller('adminController', ['$scope', '$http', function ($scope, $http) {
+        var rest = this;
+        rest.responsebody = [];
+        loadAllCountries = function ()
+        {
+            $http({method: 'GET', url: 'http://localhost:8080/country/all'})
+                    .then(function (response)
+                    {
+                        rest.responsebody = response.data;
+                        console.log(rest.responsebody);
+                    }, function (response)
+                    {
+                    });
+        };
     }]);

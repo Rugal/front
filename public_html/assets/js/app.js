68ae382b75b11179d7caed2bc8f3308e5e0ca3b0
Rugal <rugal.bernstein.0@gmail.com>
2016-02-14 13:47:12 -0500

    dynamic dropdown for materialized select

--- public_html/assets/js/app.js
+++ public_html/assets/js/app.js
@@ -1,26 +1,28 @@
-imsApp = angular.module('imsApp', []);
+imsApp = angular.module('imsApp', ['ui.materialize']);
 
 
 imsApp.controller('indexController', ['$scope', '$http', function ($scope, $http) {
-        $scope.student = 'student.html';
-        $scope.admin = 'admin.html';
-        $scope.login = 'login.html';
-        $scope.current = $scope.admin;
+        $scope.pages = ['login.html', 'student.html', 'admin.html'];
+        $scope.current = $scope.pages[2];
+        $scope.allCountries = [];
+        $scope.nationality = null;
+        $scope.loadAllCountries = function ()
+        {
+            if ($scope.allCountries.length === 0)
+            {
+                $http({method: 'GET', url: 'http://localhost:8080/country/all'})
+                        .then(function (response)
+                        {
+                            $scope.allCountries = response.data.data;
+                        }, function (response) {});
+            }
+        };
     }]);
 
 
 imsApp.controller('adminController', ['$scope', '$http', function ($scope, $http) {
-        var rest = this;
-        rest.responsebody = [];
-        $scope.loadAllCountries = function ()
+        $scope.renderCountries = function ()
         {
-            $http({method: 'GET', url: 'http://localhost:8080/country/all'})
-                    .then(function (response)
-                    {
-                        rest.responsebody = response.data;
-                        console.log(rest.responsebody);
-                    }, function (response)
-                    {
-                    });
+            $scope.loadAllCountries();
         };
     }]);

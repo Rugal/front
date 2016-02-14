c9ccfabbc8ccbee188b2c8fcc142161851ad6c85
Rugal <rugal.bernstein.0@gmail.com>
2016-02-14 14:27:25 -0500

    personal information js completed

--- public_html/assets/js/app.js
+++ public_html/assets/js/app.js
@@ -4,16 +4,24 @@ imsApp = angular.module('imsApp', ['ui.materialize']);
 imsApp.controller('indexController', ['$scope', '$http', function ($scope, $http) {
         $scope.pages = ['login.html', 'student.html', 'admin.html'];
         $scope.current = $scope.pages[2];
-        $scope.allCountries = [];
-        $scope.nationality = null;
+        $scope.preload =
+                {
+                    allCountries: []
+                };
+        $scope.predicates =
+                {
+                    nationality: 0,
+                    gender: '0',
+                    status: '0'
+                };
         $scope.loadAllCountries = function ()
         {
-            if ($scope.allCountries.length === 0)
+            if ($scope.preload.allCountries.length === 0)
             {
                 $http({method: 'GET', url: 'http://localhost:8080/country/all'})
                         .then(function (response)
                         {
-                            $scope.allCountries = response.data.data;
+                            $scope.preload.allCountries = response.data.data;
                         }, function (response) {});
             }
         };
